fbc15d7a4a9e983936dfac5f7a4f7bddda4dc87b
Paweł Przytarski <pawelprzytarski@hotmail.com>
2017-05-02 21:44:53 +0200

    Added GameEngine

--- ServerLibrary/GameEngine/GameEngine.h
+++ ServerLibrary/GameEngine/GameEngine.h
@@ -17,6 +17,10 @@ namespace Pong
 		private:
 			std::vector<std::shared_ptr<GameObject>> allObjects;
 			shared_mutex_lock_priority objectsMutex;
+			bool working;
+			bool pause;
+			std::vector<std::shared_ptr<std::thread>> threads;
+			std::chrono::milliseconds viewCooldown, physicCooldown, scriptCooldown;
 
 		private:
 			Corners getShiftedCorners(GameObject* obj, Pointf shift);
@@ -27,7 +31,18 @@ namespace Pong
 			bool checkIfPositionsAreEqual(GameObject* obj, Corners cornersOfOcject);
 
 		public:
-			virtual ~GameEngine() = default;
+			GameEngine()
+			{
+				viewCooldown = std::chrono::milliseconds(100);
+				physicCooldown = std::chrono::milliseconds(10);
+				scriptCooldown = std::chrono::milliseconds(10);
+			}
+
+			virtual ~GameEngine()
+			{
+				Stop();
+			}
+
 			bool WillCollide(std::shared_ptr<GameObject> obj, Pointf shift);
 			bool MoveObject(std::shared_ptr<GameObject> obj, Pointf shift);
 
@@ -50,6 +65,18 @@ namespace Pong
 				this->allObjects.push_back(obj);
 			}
 
+			void RemoveObject(std::shared_ptr<GameObject> obj)
+			{
+				std::lock_guard<shared_mutex_lock_priority> lock(objectsMutex);
+				for(auto it=allObjects.begin();it!=allObjects.end();it++)
+					if(*it==obj)
+					{
+						allObjects.erase(it);
+						break;
+					}
+			}
+
+		protected:
 			virtual void DoScripts()
 			{
 				std::shared_lock<shared_mutex_lock_priority> lock(objectsMutex);
@@ -72,6 +99,60 @@ namespace Pong
 			{
 				std::shared_lock<shared_mutex_lock_priority> lock(objectsMutex);
 				view->DisplayObjects(allObjects);
+			}
+
+		public:
+			void Start(std::shared_ptr<View> view)
+			{
+				working = true;
+				pause = false;
+
+				threads.push_back(std::make_shared<std::thread>([&]()
+				{
+					while (working && !pause)
+					{
+						DoPhysic();
+						std::this_thread::sleep_for(physicCooldown);
+					}
+				}));
+
+				threads.push_back(std::make_shared<std::thread>([&]()
+				{
+					while (working)
+					{
+						DoView(view);
+						std::this_thread::sleep_for(viewCooldown);
+					}
+				}));
+
+				threads.push_back(std::make_shared<std::thread>([&]()
+				{
+					while (working &&!pause)
+					{
+						DoScripts();
+						std::this_thread::sleep_for(scriptCooldown);
+					}
+				}));
+			}
+
+			void Pause()
+			{
+				pause = true;
+			}
+
+			void Resume()
+			{
+				pause = false;
+			}
+
+			void Stop()
+			{
+				working = false;
+				for (auto t : threads)
+				{
+					if (t->joinable())
+						t->join();
+				}
 			}
 		};
 
